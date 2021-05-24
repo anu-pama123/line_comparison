2896eecc162428b4b4692a092e6e1e407a2ced36
anupama cv <anu.anupamacv@gmail.com>
2021-05-24 08:19:25 +0530

    ADDED: functionality to check length of line

--- src/main/java/com/bridgelabz/LineComparisonBuilder.java
+++ src/main/java/com/bridgelabz/LineComparisonBuilder.java
@@ -1,9 +1,26 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class LineComparisonBuilder
 {
-    public static void main(String[] args)
+    private static Scanner obj;
+    private double length;
+    public void length_of_line(int x1, int x2, int y1, int y2)
     {
-        System.out.println("Welcome to line comparison computation program");
+        this.length=Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        return;
+    }
+    public static void main(String[] arg)
+    {
+        obj = new Scanner(System.in);
+        LineComparisonBuilder object=new LineComparisonBuilder();
+        System.out.println("Enter the numbers x1,x2,y1,y2 ");
+        int x1, x2, y1, y2;
+        x1 = obj.nextInt();
+        x2 = obj.nextInt();
+        y1 = obj.nextInt();
+        y2 = obj.nextInt();
+        object.length_of_line(x1, x2, y1, y2);
+        System.out.println("Length of line = " + object.length);
     }
 }
